5f2698f502f7f0b8b2b65676781c7f5c5321af60
AVM82 <1981AlexVM@gmail.com>
2017-05-24 23:10:39 +0300

    Created a fight.js file and added it in the index.html file. Created .gitignore

---
+++ fight.js
@@ -0,0 +1,8 @@
+
+//let improvedFighter = new ImprovedFighter("improvedFighter", 10, 50);
+
+let fighter = new Fighter("fighter", 50, 150);
+let fight = function(fighter2, improvedFighter, ...point){
+    console.log('fighter');
+}
+
